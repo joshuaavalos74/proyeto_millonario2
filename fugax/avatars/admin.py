e492c5a7cec52ecc87a635889ceb8e8048e54bf6
joshuaavalos74 <joshuaavalos74@gmail.com>
2017-05-09 10:26:10 -0600

    Creacion de modelos: Tags, Usuario y solo.

--- fugax/avatars/admin.py
+++ fugax/avatars/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
-
+from avatars.models import Avatar 
+admin.site.register(Avatar)
 # Register your models here.
--- fugax/usuario/admin.py
+++ fugax/usuario/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from usuario.models import Perfil, UserTag
+admin.site.register(Perfil)
+admin.site.register(UserTag)
 
 # Register your models here.
