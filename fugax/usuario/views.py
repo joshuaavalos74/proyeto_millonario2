5a769a0152d40af1f66d300511195a24790edf06
Darlight <andres.perdomo09@gmail.com>
2017-05-16 11:57:50 -0600

    Mover el archivo en perfil en vez de usuario

--- fugax/usuario/views.py
+++ fugax/usuario/views.py
@@ -1,8 +1,5 @@
 from django.shortcuts import render
-from usuario.models import Perfil
 
-def perfil_especifico(request):
-	"""."""
-	perfil = Perfil.objects.get()
-	return render(request, 'lista_estudiantes.html',{'Perfil': perfil})
+
 # Create your views here.
+																																																								
