b0fb248397ea8c9a9d2fdb2593149d645bf69ca7
joshuaavalos74 <joshuaavalos74@gmail.com>
2017-06-13 10:37:26 -0600

    arreglando perfil

--- fugax/perfil/views.py
+++ fugax/perfil/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
 from usuario.models import Perfil
 # Create your views here.
-def perfil_especifico(request):
+def perfil_especifico(request, profile_pk):
 	"""."""
-	perfil = Perfil.objects.get()
+	perfil = Perfil.objects.get(pk=profile_pk)
 	return render(request, 'profile.html',{'Perfil': perfil})
 
 
